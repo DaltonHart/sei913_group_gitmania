f995d05bf96d760766a32ebc0e946ac62d7b4e2c
daltonhart <daltonvaren@gmail.com>
2021-10-13 11:21:49 -0700

    adds fun route

--- server.js
+++ server.js
@@ -37,6 +37,13 @@ app.get("/", function (req, res, next) {
 // == Makeup Routes
 app.use("/makeup", controllers.makeup);
 
+// fun route
+app.get("/help", function (req, res) {
+  res.send({
+    message: "No help was found",
+  });
+});
+
 /* === Server Listener === */
 app.listen(PORT, function () {
   console.log(`Server is live and listening at localhost:${PORT}. 🤑`);
