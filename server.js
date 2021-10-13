73d7421aaaf77e517fa54776f0f4f0c0ce67d52e
daltonhart <daltonvaren@gmail.com>
2021-10-13 10:09:58 -0700

    adds database connection

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 /* === External modules === */
 const express = require("express");
+
 /* === Internal modules === */
+
 /* === System Variables === */
 const app = express();
 const PORT = 4000;
+
 /* === Server Configuration === */
+// mongodb connection
+require("./config/db.connection");
+
 /* === Middleware === */
+
 /* === Routes === */
+
 /* === Server Listener === */
 app.listen(PORT, function () {
   console.log(`Server is live and listening at localhost:${PORT}. 🤑`);
